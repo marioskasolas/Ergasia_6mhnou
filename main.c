bfef92d086a8edf783528891bdd5dbfb338ce383
marioskasolas <marioskasolas@gmail.com>
2022-12-06 16:56:30 +0200

    6/12/2022

--- main.c
+++ main.c
@@ -1,36 +1,107 @@
 #include <stdio.h>
 
-int bin(unsigned short int hexadecimal);
+int ms_bin(unsigned short int hexadecimal);
+int case_s(unsigned short int ptr);
+int sms_bin(unsigned short int hexadecimal);
 
 int main() {
+
     /* εισαγωγή hexadecimal */
+    unsigned short int msb1,msb2;
     unsigned short int hexadecimal;
     printf("enter hexadecimal\n");
     scanf("%x", &hexadecimal);
-    printf("you entered %x\n", hexadecimal);
-    printf("%x\n", hexadecimal);
-    bin(hexadecimal);
+
+    /* χρήση συναρτήσεων για μετατροπή απο hexa σε binary */
+    msb1 = ms_bin(hexadecimal);
+    printf("%x ptr1\n",msb1);
+    msb2 = sms_bin(hexadecimal);
+    printf("%x ptr2\n",msb2);
+
+    /* υπολογισμός ηλικίας μεσώ των  7msb */
+    int tran1;
+    tran1 = (int)msb1 * 1000;
+    printf("%d tran1",tran1);
+
+
 
 }
+       /* συνάρτηση για μετατροπή δεκαεξαδικού σε binary */
+        int case_s(unsigned short int ptr) {
+             int temp;
+            switch(ptr) {
+                case 0:
+                    temp = 0b0000;
+                    break;
+                case 1:
+                    temp = 0b0001;
+                    break;
+                case 2:
+                    temp = 0b0010;
+                    break;
+                case 3:
+                    temp = 0b0011;
+                    break;
+                case 4:
+                    temp = 0b0100;
+                    break;
+                case 5:
+                    temp = 0b0101;
+                    break;
+                case 6:
+                    temp = 0b0110;
+                    break;
+                case 7:
+                    temp = 0b0111;
+                    break;
+                case 8:
+                    temp = 0b1000;
+                    break;
+                case 9:
+                    temp = 0b1001;
+                    break;
+                case 0xA:
+                    temp = 0b1010;
+                    break;
+                case 0xB:
+                    temp = 0b1011;
+                    break;
+                case 0xc:
+                    temp = 0b1100;
+                    break;
+                case 0xD:
+                    temp = 0b1101;
+                    break;
+                case 0xe:
+                    temp = 0b1110;
+                    break;
+                case 0xf:
+                    temp = 0b1111;
+                    break;
+            }
+            return temp;
+        }
 
 
-    int bin(unsigned short int hexadecimal){
+
+
+        /* συνάρτηση που χωρίζει τα πρώτα 4 most significant bits */
+       int ms_bin(unsigned short int hexadecimal){
        int i;
-       unsigned short int ptr1,ptr2,ptr3,ptr4;
+       unsigned short int ptr1;
 
-       ptr1 = (hexadecimal >> 12) ;
-       printf("%x ptr1\n",ptr1);
+       ptr1 = (hexadecimal >> 12);
+       ptr1 = case_s(ptr1);
+       return  ptr1;
 
+       }
 
-       ptr2 = (hexadecimal >> 8);
-       ptr2 = (ptr2 << 4) & 0xf;
-       printf("%x ptr2\n",ptr2);
+       /* συνάρτηση που χωρίζει τα δεύτερα  most significant bits δηλαδή τα 4 επομένα των πρώτων 4ρών*/
+       int sms_bin(unsigned short int hexadecimal){
+           unsigned short int ptr2;
 
-       ptr3 = (hexadecimal >> 4);
-       ptr3 = (ptr3 << 8) & 0xf00;
-       printf("%x ptr3\n",ptr3);
-
-       ptr4 = (hexadecimal >> 12);
-       ptr4 = (ptr4 << 12) & 0xffff ;
-       printf("%x ptr4\n",ptr4);
-}
+           ptr2 = (hexadecimal >> 8) & 0x0f;
+           /*ptr2 = case_s(ptr2);*/
+           ptr2 = (ptr2 >>1 );
+           return ptr2;
+       }
