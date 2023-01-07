3e4bade82e51c62a6f79ff9d6dbb794498899830
marioskasolas <marioskasolas@gmail.com>
2023-01-07 17:14:01 +0200

    7/1 malloc anti calloc kai n ==0 anti n == 1 kanei return 0;

--- main.c
+++ main.c
@@ -13,11 +13,11 @@ void bubblesort(int**,int);
 
 int main() {
     unsigned int cand[NUM_CANDIDATES][3];
-    int age,gender,vote,i,**p,n = 1;
+    int age,gender,vote,i,**p,n = 0;
     char c[5];
     c[5] = '\0';
 
-    //ανοίγμα αρχείου ψηφοφόρων
+    //áíïßãìá áñ÷åßïõ øçöïöüñùí
     FILE * fp;
     fp = fopen("C:/Users/mario/OneDrive/Desktop/writeup.dat","r");
 
@@ -26,13 +26,13 @@ int main() {
         for(i = 1;i <= 4;i++){
             c[i] = fgetc(fp);
         }
-        //χρήση συνάρτησης η οποία λαμβάνει ως ορίσματα τα πρώτα 2 ψηφία του δεκαεξαδικού και τα μετατρέπει σε ηλικία
+        //÷ñÞóç óõíÜñôçóçò ç ïðïßá ëáìâÜíåé ùò ïñßóìáôá ôá ðñþôá 2 øçößá ôïõ äåêáåîáäéêïý êáé ôá ìåôáôñÝðåé óå çëéêßá
         age = age_converter(c[1],c[2]);
 
-        //χρήση συνάρτησης για μετατροπή του γένους απο το 8ό και το 9ό μπιτ
+        //÷ñÞóç óõíÜñôçóçò ãéá ìåôáôñïðÞ ôïõ ãÝíïõò áðï ôï 8ü êáé ôï 9ü ìðéô
         gender = gender_converter(c[2],c[3]);
 
-        //χρήση συνάρτησης η οποια λαμβάνει ώς όρισμα το τρίτο και τέταρτο ψηφίο ενος δεκαεξαδικού και το μετατρέπει στην ψήφο του ψηφοφόρου
+        //÷ñÞóç óõíÜñôçóçò ç ïðïéá ëáìâÜíåé þò üñéóìá ôï ôñßôï êáé ôÝôáñôï øçößï åíïò äåêáåîáäéêïý êáé ôï ìåôáôñÝðåé óôçí øÞöï ôïõ øçöïöüñïõ
         vote = vote_converter(c[3], c[4]);
         printf("%d vote,%d age,%d gender\n",vote,age,gender);
 
@@ -40,15 +40,18 @@ int main() {
             printf("This voters vote is valid!\n");
             printf("\n");
 
-            //συμπλήρωση δυναμικού πίνακα
-            if(n == 1){
-                p = calloc(n, sizeof(int*));
-                p[n] = calloc(3,sizeof(int));
+            //óõìðëÞñùóç äõíáìéêïý ðßíáêá
+            if(n == 0){
+                p = (int**) malloc(1 * sizeof(int*));
+                p[n] = (int *) malloc(3 * sizeof(int));
+                printf("%p",p);
             }
-            else if(n != 1 ){
-                p = realloc(p,n*sizeof(int));
-                p[n] = calloc(3,sizeof(int));
+            else{
+                p = realloc(p,sizeof(int*));
+                p[n] = (int *) malloc(3 * sizeof(int));
+                printf("%p",p);
             }
+            if(fp == NULL) printf("NULL");
 
             p[n][1] = age;
             p[n][2] = gender;
@@ -71,110 +74,112 @@ int main() {
         }
     }
 */
- }
+}
 
 
 
 
-// συνάρτηση που υπολογιζεί μεσώ των πρώτων 2 ψηφιών του δεκαεξαδικου αριθμού την ηλικία του ψηφοφόρου
-    int age_converter(char first_hex, char second_hex){
-        //first_hex αναπαραστά το πρώτο ψηφίο του 16δικου, second_hex αναπαραστά το δεύτερο ψηφίο του 16δικού
-       unsigned short int first1_hex, second2_hex;
-       first1_hex = hex_conv(first_hex);
-       second2_hex = hex_conv(second_hex);
+// óõíÜñôçóç ðïõ õðïëïãéæåß ìåóþ ôùí ðñþôùí 2 øçöéþí ôïõ äåêáåîáäéêïõ áñéèìïý ôçí çëéêßá ôïõ øçöïöüñïõ
+int age_converter(char first_hex, char second_hex){
+    //first_hex áíáðáñáóôÜ ôï ðñþôï øçößï ôïõ 16äéêïõ, second_hex áíáðáñáóôÜ ôï äåýôåñï øçößï ôïõ 16äéêïý
+    unsigned short int first1_hex, second2_hex;
+    first1_hex = hex_conv(first_hex);
+    second2_hex = hex_conv(second_hex);
 
-       second2_hex = second2_hex >> 1;
+    second2_hex = second2_hex >> 1;
 
 
-        int r;//remainder του χ/2
-        int sum = 0;//αποθηκεύει την ηλικία
-        for (int i = 0; i <= 2; i++) {
-            r = second2_hex % 2;
-            //printf("r1 is %d ",r);
-            second2_hex = second2_hex / 2;
-            //printf("hex1 is %d",second2_hex);
-            if(r == 1) {
-                sum = sum + pow(2, i);
-                //printf("sum1 is %d\n",sum);
+    int r;//remainder ôïõ ÷/2
+    int sum = 0;//áðïèçêåýåé ôçí çëéêßá
+    int i;
+    for (i = 0; i <= 2; i++) {
+        r = second2_hex % 2;
+        //printf("r1 is %d ",r);
+        second2_hex = second2_hex / 2;
+        //printf("hex1 is %d",second2_hex);
+        if(r == 1) {
+            sum = sum + pow(2, i);
+            //printf("sum1 is %d\n",sum);
+        }
+    }
+
+
+    for (i = 3; i <= 6; i++) {
+        r = first1_hex % 2;
+        //printf("r2 is %d ",r);
+        first1_hex = first1_hex / 2;
+        //printf("hex2 is %d ",second2_hex);
+        if (r == 1) {
+            sum = sum + pow(2, i);
+            //printf("sum2 is %d\n",sum);
+        }
+    }
+
+    return sum;
+}
+
+// óõíÜñôçóç ðïõ ìåôáôñÝðåé ôá bit ðïõ áíôéðñïóùðÝõïõí ôïí öýëï óôïí áíôéóïß÷ï áñéèìü
+int gender_converter(char second_hex,char third_hex) {
+    //printf("second hex digit: %c, third hex digit: %c\n",second_hex,third_hex);
+    unsigned short int second2_hex, third3_hex;
+
+    //êÜíåé isolate ôï ôåëåõôáßï ìðéô ôïõ äåýôåñïõ øçößïõ ôïõ äåêáåîáäéêïý
+    second2_hex = hex_conv(second_hex);
+    second2_hex = second2_hex & 0x0001;
+    if (second2_hex == 1) {
+        second2_hex = 2;
+    }
+
+
+    //êÜíåé isolate ôï ðñþôï ìðéô ôïõ 3ïý øçößïõ ôïõ äåêáåîáäéêïý
+    third3_hex = hex_conv(third_hex);
+    third3_hex = third3_hex >> 3;
+    //printf("third3 is : %x\n",third3_hex);
+    if (third3_hex == 1) {
+        third3_hex = 1;
+    }
+
+
+    return second2_hex + third3_hex;
+}
+
+int vote_converter(char third_hex, char fourth_hex) {
+    unsigned short int fourth4_hex, third3_hex;//temp1 = fourth , temp2 = third
+    fourth4_hex = hex_conv(fourth_hex);
+    int consec = 0;
+    int r,i, index;
+    for( i = 0; i <= 3; i++) {
+        r = fourth4_hex % 2;
+        fourth4_hex = fourth4_hex / 2;
+        if (r == 1) {
+            consec = consec + 1;
+            index = i;
+            if (consec > 1) {
+                index = -1;
+                break;
             }
         }
 
-        for (int i = 3; i <= 6; i++) {
-            r = first1_hex % 2;
-            //printf("r2 is %d ",r);
-            first1_hex = first1_hex / 2;
-            //printf("hex2 is %d ",second2_hex);
-            if (r == 1) {
-                sum = sum + pow(2, i);
-                //printf("sum2 is %d\n",sum);
-            }
-        }
-
-        return sum;
     }
-
-    // συνάρτηση που μετατρέπει τα bit που αντιπροσωπέυουν τον φύλο στον αντισοίχο αριθμό
-    int gender_converter(char second_hex,char third_hex) {
-        //printf("second hex digit: %c, third hex digit: %c\n",second_hex,third_hex);
-        unsigned short int second2_hex, third3_hex;
-
-        //κάνει isolate το τελευταίο μπιτ του δεύτερου ψηφίου του δεκαεξαδικού
-        second2_hex = hex_conv(second_hex);
-        second2_hex = second2_hex & 0x0001;
-        if (second2_hex == 1) {
-            second2_hex = 2;
-        }
-
-
-        //κάνει isolate το πρώτο μπιτ του 3ού ψηφίου του δεκαεξαδικού
-        third3_hex = hex_conv(third_hex);
-        third3_hex = third3_hex >> 3;
-        //printf("third3 is : %x\n",third3_hex);
-        if (third3_hex == 1) {
-            third3_hex = 1;
-        }
-
-
-        return second2_hex + third3_hex;
-    }
-
-    int vote_converter(char third_hex, char fourth_hex) {
-        unsigned short int fourth4_hex, third3_hex;//temp1 = fourth , temp2 = third
-        fourth4_hex = hex_conv(fourth_hex);
-        int consec = 0;
-        int r, index;
-        for (int i = 0; i <= 3; i++) {
-            r = fourth4_hex % 2;
-            fourth4_hex = fourth4_hex / 2;
+    third3_hex = hex_conv(third_hex);
+    if (index != -1) {
+        third3_hex = third3_hex & 0x0111;
+        for ( i = 4; i <= 6; i++) {
+            r = third3_hex % 2;
+            third3_hex = third3_hex / 2;
             if (r == 1) {
                 consec = consec + 1;
                 index = i;
+                //printf("consec %d\n", consec);
                 if (consec > 1) {
                     index = -1;
                     break;
                 }
             }
-
         }
-        third3_hex = hex_conv(third_hex);
-        if (index != -1) {
-            third3_hex = third3_hex & 0x0111;
-            for (int i = 4; i <= 6; i++) {
-                r = third3_hex % 2;
-                third3_hex = third3_hex / 2;
-                if (r == 1) {
-                    consec = consec + 1;
-                    index = i;
-                    //printf("consec %d\n", consec);
-                    if (consec > 1) {
-                        index = -1;
-                        break;
-                    }
-                }
-            }
-        }
-        return index;
     }
+    return index;
+}
 
 
 unsigned short int hex_conv(char c) {
@@ -256,11 +261,11 @@ void bubblesort(int** p,int n){
 
 void candidates_table(unsigned int cand[6][3],int** p,int n) {
     printf("in function\n");
-    int i;
+    int i,j;
 
-    //μηδενισμός πίνακα
+    //ìçäåíéóìüò ðßíáêá
     for(i = 0; i <= 6; i++){
-        for(int j = 1; j <= 3; j++){
+        for( j = 1; j <= 3; j++){
             cand[i][j] = 0;
         }
     }
@@ -269,6 +274,6 @@ void candidates_table(unsigned int cand[6][3],int** p,int n) {
         printf("i is : %d ,",i);
         cand[p[i][3]] [p[i][2]]++;
         printf(" cand is %d\n",cand[p[i][3]] [p[i][2]]);
-        }
+    }
 
 }
